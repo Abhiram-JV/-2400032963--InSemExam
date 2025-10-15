c7db7a70c894221ec270c29ec382998cea1c0f36
Abhiram-JV <jalduvabhiram@gmail.com>
2025-10-15 17:21:40 +0530

    Setup for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({Replace with your actual repo 
+export default defineConfig({
   base: '/-2400032963--InSemExam/',  
   plugins: [react()],
 })
